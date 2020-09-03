0fd452d9a8104a15ef925a0c12de82e4a64c4399
Ugozeal <okonkwodavidu@gmail.com>
2020-09-03 16:52:41 +0100

    adding lisence plate and palindrome algorithm

---
+++ LicensePLateConversion.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+func similarLicensePlates(_ plate1: String,_ plate2: String) -> Bool {
+
+    var plate11 = ""
+    var plate22 = ""
+    for item in plate1 {
+        if item == "0" || item == "O" || item == "Q" {
+            let item = "0"
+            plate11.append(item)
+            print(item)
+        }else if item == "1" || item == "I" || item == "T"{
+            let item = "T"
+            plate11.append(item)
+            print(item)
+        }else if item == "2" || item == "Z" {
+            let item = "Z"
+            plate11.append(item)
+            print(item)
+        }else if item == "5" || item == "S" {
+            let item = "S"
+            plate11.append(item)
+            print(item)
+        }else if item == "8" || item == "B" {
+            let item = "B"
+            plate11.append(item)
+            print(item)
+        }
+
+    }
+    for item in plate2 {
+          if item == "0" || item == "O" || item == "Q" {
+              let item = "0"
+            plate22.append(item)
+          }else if item == "1" || item == "I" || item == "T"{
+              let item = "T"
+            plate22.append(item)
+          }else if item == "2" || item == "Z" {
+              let item = "Z"
+            plate22.append(item)
+          }else if item == "5" || item == "S" {
+              let item = "S"
+            plate22.append(item)
+          }else if item == "8" || item == "B" {
+              let item = "B"
+            plate22.append(item)
+              print(item)
+          }
+      }
+    if plate11 == plate22{
+        return true
+    }else{
+        return false
+    }
+   return true
+}
+
---
+++ PalindromeChainLength.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+
+func palindromeChainLength(_ num: Int) -> Int {
+     let convNumToString = String(num)
+    let reversed = String(convNumToString.reversed())
+     let changeReversedBackToInt = Int(reversed)!
+     var count = 0
+     var sum : Int
+     while count < convNumToString.count {
+         count = 0
+         sum = -1
+         if reversed == convNumToString {
+           return sum + 1
+         }
+         count = count + 1
+         sum += 1
+         let check = palindromeChainLength(num + changeReversedBackToInt) + count
+         return check
+     }
+    return count
+}
+
+
+
+palindromeChainLength(89)
+
