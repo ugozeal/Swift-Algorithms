b2e93d27bb498ada7ffa09b48eb59c66b05fc43c
David Okonkwo <okonkwodavidu@gmail.com>
2023-02-18 18:31:31 +0100

    update

--- GetPasswordStrength.playground/Contents.swift
+++ GetPasswordStrength.playground/Contents.swift
@@ -9,38 +9,61 @@ import UIKit
  *  2. STRING_ARRAY common_words
  */
 
+//func getPasswordStrength(passwords: [String], common_words: [String]) -> [String] {
+//    // Write your code here
+//    let n = passwords.count
+//    let m = common_words.count
+//    var result = Array(repeating: "strong", count: n)
+//
+//    for i in 0..<n {
+//        let password = passwords[i]
+//        if passwords.count < 6 {
+//            result[i] = "weak"
+//        } else if
+//            password.rangeOfCharacter(from: .decimalDigits) != nil &&
+//                password.rangeOfCharacter(from: .lowercaseLetters) == nil &&
+//                password.rangeOfCharacter(from: .uppercaseLetters) == nil {
+//            result[i] = "weak"
+//        } else if password.rangeOfCharacter(from: .lowercaseLetters) != nil &&
+//                    password.rangeOfCharacter(from: .decimalDigits) == nil &&
+//                    password.rangeOfCharacter(from: .uppercaseLetters) == nil {
+//            result[i] = "weak"
+//        } else if password.rangeOfCharacter(from: .uppercaseLetters) != nil &&
+//                    password.rangeOfCharacter(from: .decimalDigits) == nil &&
+//                    password.rangeOfCharacter(from: .lowercaseLetters) == nil {
+//            result[i] = "weak"
+//        } else {
+//            result[i] = "strong"
+//        }
+//
+//        for j in 0..<m {
+//            if password.contains(common_words[j]) {
+//                result[i] = "weak"
+//                break
+//            }
+//        }
+//    }
+//    return result
+//}
 func getPasswordStrength(passwords: [String], common_words: [String]) -> [String] {
-    // Write your code here
-    let n = passwords.count
-    let m = common_words.count
-    var result = Array(repeating: "strong", count: n)
-    
-    for i in 0..<n {
-        let password = passwords[i]
-        if passwords.count < 6 {
+    var result = Array(repeating: "strong", count: passwords.count)
+
+    for i in 0..<passwords.count {
+        if passwords[i].count < 6 || passwords[i].allSatisfy({ $0.isUppercase }) {
             result[i] = "weak"
-        } else if
-            password.rangeOfCharacter(from: .decimalDigits) != nil &&
-                password.rangeOfCharacter(from: .lowercaseLetters) == nil &&
-                password.rangeOfCharacter(from: .uppercaseLetters) == nil {
-            result[i] = "weak"
-        } else if password.rangeOfCharacter(from: .lowercaseLetters) != nil &&
-                    password.rangeOfCharacter(from: .decimalDigits) == nil &&
-                    password.rangeOfCharacter(from: .uppercaseLetters) == nil {
-            result[i] = "weak"
-        } else if password.rangeOfCharacter(from: .uppercaseLetters) != nil &&
-                    password.rangeOfCharacter(from: .decimalDigits) == nil &&
-                    password.rangeOfCharacter(from: .lowercaseLetters) == nil {
-            result[i] = "weak"
-        } else {
-            result[i] = "strong"
+            continue
         }
         
-        for j in 0..<m {
-            if password.contains(common_words[j]) {
-                result[i] = "weak"
-                break
-            }
+        if passwords.count > 20 {
+            var result = Array(repeating: "weak", count: passwords.count)
+            return result
+        }
+       
+       let containCommonWord = common_words.contains { commonWord in
+           return passwords[i].range(of: commonWord, options: .caseInsensitive) != nil
+       }
+        if containCommonWord {
+            result[i] = "weak"
         }
     }
     return result
